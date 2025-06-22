73eef70dbb754523303f6692092c945b881a2333
Abubakar Abid <abubakar@huggingface.co>
2025-06-22 09:34:34 -0700

    changes

--- trackio/imports.py
+++ trackio/imports.py
@@ -108,7 +108,7 @@ def import_csv(
         utils.print_dashboard_instructions(project)
     else:
         deploy.create_space_if_not_exists(space_id, dataset_id)
-        # deploy.upload_db_to_space(project, space_id)
+        # deploy.upload_db_to_space(project, space_id) # TODO: uncomment this
         print(
             f"* View dashboard by going to: {deploy.SPACE_URL.format(space_id=space_id)}"
         )
