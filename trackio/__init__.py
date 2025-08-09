0152331e86375b1a8187bd6944884bdf322775d0
UNI <52519513+universuen@users.noreply.github.com>
2025-08-10 03:07:34 +0800

    bugfix for issue #125 (#126)
    * bugfix
    
    * changes
    
    * added test
    
    ---------
    
    Co-authored-by: Abubakar Abid <abubakar@huggingface.co>

--- trackio/__init__.py
+++ trackio/__init__.py
@@ -129,8 +129,11 @@ def log(metrics: dict, step: int | None = None) -> None:
     """
     run = context_vars.current_run.get()
     if run is None:
-        raise RuntimeError("Call trackio.init() before log().")
-    run.log(metrics)
+        raise RuntimeError("Call trackio.init() before trackio.log().")
+    run.log(
+        metrics=metrics,
+        step=step,
+    )
 
 
 def finish():
@@ -139,7 +142,7 @@ def finish():
     """
     run = context_vars.current_run.get()
     if run is None:
-        raise RuntimeError("Call trackio.init() before finish().")
+        raise RuntimeError("Call trackio.init() before trackio.finish().")
     run.finish()
 
 
