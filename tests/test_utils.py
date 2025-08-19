dd5b232d06ae67b182cea6e672594c5f47b61751
Abubakar Abid <abubakar@huggingface.co>
2025-08-19 15:26:03 -0400

    rm unncessary test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,4 @@
 import random
-from unittest.mock import patch
 
 from trackio import utils
 
@@ -10,26 +9,3 @@ def test_generate_readable_names_are_unique_even_with_seed():
         random.seed(42)
         names.append(utils.generate_readable_name(names))
     assert len(names) == len(set(names))
-
-
-@patch("huggingface_hub.whoami")
-@patch("time.time")
-def test_generate_readable_name_with_space_id(mock_time, mock_whoami):
-    mock_whoami.return_value = {"name": "testuser"}
-    mock_time.return_value = 1234567890
-
-    name = utils.generate_readable_name(
-        ["existing_name"], space_id="testuser/test-space"
-    )
-    assert name == "testuser-1234567890"
-
-    name2 = utils.generate_readable_name(
-        ["testuser-1234567890"], space_id="testuser/test-space"
-    )
-    assert name2 == "testuser-1234567890"
-
-
-def test_generate_readable_name_without_space_id():
-    name = utils.generate_readable_name([])
-    assert "-" in name
-    assert name.count("-") == 2
