9487d1d779fb3ba746cf41b047e4c35065360714
Abubakar Abid <abubakar@huggingface.co>
2025-08-22 11:52:36 -0400

    handle persistent storage

--- trackio/ui.py
+++ trackio/ui.py
@@ -25,8 +25,14 @@ except:  # noqa: E722
 
 
 def get_project_info() -> str | None:
+    info = None
     dataset_id = os.environ.get("TRACKIO_DATASET_ID")
     space_id = os.environ.get("SPACE_ID")
+    persistent_storage_enabled = os.environ.get(
+        "PERSISTANT_STORAGE_ENABLED"  # Space env name has a typo
+    )
+    if persistent_storage_enabled:
+        info = "&#10024; Persistent Storage is enabled, logs are stored directly in this Space. To avoid losing data between syncs, <a href='https://huggingface.co/spaces/{space_id}/settings' class='accent-link'>click here</a> to open this Space's settings and add Persistent Storage."
     if dataset_id:
         sync_status = utils.get_sync_status(SQLiteStorage.get_scheduler())
         upgrade_message = f"New changes are synced every 5 min <span class='info-container'><input type='checkbox' class='info-checkbox' id='upgrade-info'><label for='upgrade-info' class='info-icon'>&#9432;</label><span class='info-expandable'> To avoid losing data between syncs, <a href='https://huggingface.co/spaces/{space_id}/settings' class='accent-link'>click here</a> to open this Space's settings and add Persistent Storage.</span></span>"
@@ -34,8 +40,6 @@ def get_project_info() -> str | None:
             info = f"&#x21bb; Backed up {sync_status} min ago to <a href='https://huggingface.co/datasets/{dataset_id}' target='_blank' class='accent-link'>{dataset_id}</a> | {upgrade_message}"
         else:
             info = f"&#x21bb; Not backed up yet to <a href='https://huggingface.co/datasets/{dataset_id}' target='_blank' class='accent-link'>{dataset_id}</a> | {upgrade_message}"
-    else:
-        info = None
     return info
 
 
