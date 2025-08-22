393df5e15b80bbfe3d03efa0407855a4cf292ce3
Abubakar Abid <abubakar@huggingface.co>
2025-08-22 07:35:13 -0400

    revert

--- trackio/__init__.py
+++ trackio/__init__.py
@@ -1,19 +1,21 @@
 import os
+import warnings
 import webbrowser
 from pathlib import Path
+from typing import Any
 
 from gradio_client import Client
 
 from trackio import context_vars, deploy, utils
-from trackio.imports import import_csv
+from trackio.imports import import_csv, import_tf_events
 from trackio.run import Run
 from trackio.sqlite_storage import SQLiteStorage
 from trackio.ui import demo
-from trackio.utils import TRACKIO_DIR, TRACKIO_LOGO_PATH
+from trackio.utils import TRACKIO_DIR, TRACKIO_LOGO_DIR
 
 __version__ = Path(__file__).parent.joinpath("version.txt").read_text().strip()
 
-__all__ = ["init", "log", "finish", "show", "import_csv"]
+__all__ = ["init", "log", "finish", "show", "import_csv", "import_tf_events"]
 
 
 config = {}
@@ -26,6 +28,7 @@ def init(
     dataset_id: str | None = None,
     config: dict | None = None,
     resume: str = "never",
+    settings: Any = None,
 ) -> Run:
     """
     Creates a new Trackio project and returns a Run object.
@@ -34,20 +37,32 @@ def init(
         project: The name of the project (can be an existing project to continue tracking or a new project to start tracking from scratch).
         name: The name of the run (if not provided, a default name will be generated).
         space_id: If provided, the project will be logged to a Hugging Face Space instead of a local directory. Should be a complete Space name like "username/reponame" or "orgname/reponame", or just "reponame" in which case the Space will be created in the currently-logged-in Hugging Face user's namespace. If the Space does not exist, it will be created. If the Space already exists, the project will be logged to it.
-        dataset_id: If provided, a persistent Hugging Face Dataset will be created and the metrics will be synced to it every 5 minutes. Should be a complete Dataset name like "username/datasetname" or "orgname/datasetname", or just "datasetname" in which case the Dataset will be created in the currently-logged-in Hugging Face user's namespace. If the Dataset does not exist, it will be created. If the Dataset already exists, the project will be appended to it. If not provided, the metrics will be logged to a local SQLite database, unless a `space_id` is provided, in which case a Dataset will be automatically created with the same name as the Space but with the "_dataset" suffix.
+        dataset_id: If a space_id is provided, a persistent Hugging Face Dataset will be created and the metrics will be synced to it every 5 minutes. Specify a Dataset with name like "username/datasetname" or "orgname/datasetname", or "datasetname" (uses currently-logged-in Hugging Face user's namespace), or None (uses the same name as the Space but with the "_dataset" suffix). If the Dataset does not exist, it will be created. If the Dataset already exists, the project will be appended to it.
         config: A dictionary of configuration options. Provided for compatibility with wandb.init()
         resume: Controls how to handle resuming a run. Can be one of:
             - "must": Must resume the run with the given name, raises error if run doesn't exist
             - "allow": Resume the run if it exists, otherwise create a new run
             - "never": Never resume a run, always create a new one
+        settings: Not used. Provided for compatibility with wandb.init()
     """
+    if settings is not None:
+        warnings.warn(
+            "* Warning: settings is not used. Provided for compatibility with wandb.init(). Please create an issue at: https://github.com/gradio-app/trackio/issues if you need a specific feature implemented."
+        )
+
+    if space_id is None and dataset_id is not None:
+        raise ValueError("Must provide a `space_id` when `dataset_id` is provided.")
     space_id, dataset_id = utils.preprocess_space_and_dataset_ids(space_id, dataset_id)
     url = context_vars.current_server.get()
 
     if url is None:
         if space_id is None:
             _, url, _ = demo.launch(
-                show_api=False, inline=False, quiet=True, prevent_thread_lock=True
+                show_api=False,
+                inline=False,
+                quiet=True,
+                prevent_thread_lock=True,
+                show_error=True,
             )
         else:
             url = space_id
@@ -98,31 +113,38 @@ def init(
         client=client,
         name=name,
         config=config,
+        space_id=space_id,
     )
     context_vars.current_run.set(run)
     globals()["config"] = run.config
     return run
 
 
-def log(metrics: dict) -> None:
+def log(metrics: dict, step: int | None = None) -> None:
     """
     Logs metrics to the current run.
 
     Args:
         metrics: A dictionary of metrics to log.
+        step: The step number. If not provided, the step will be incremented automatically.
     """
-    if context_vars.current_run.get() is None:
-        raise RuntimeError("Call trackio.init() before log().")
-    context_vars.current_run.get().log(metrics)
+    run = context_vars.current_run.get()
+    if run is None:
+        raise RuntimeError("Call trackio.init() before trackio.log().")
+    run.log(
+        metrics=metrics,
+        step=step,
+    )
 
 
 def finish():
     """
     Finishes the current run.
     """
-    if context_vars.current_run.get() is None:
-        raise RuntimeError("Call trackio.init() before finish().")
-    context_vars.current_run.get().finish()
+    run = context_vars.current_run.get()
+    if run is None:
+        raise RuntimeError("Call trackio.init() before trackio.finish().")
+    run.finish()
 
 
 def show(project: str | None = None):
@@ -137,8 +159,8 @@ def show(project: str | None = None):
         quiet=True,
         inline=False,
         prevent_thread_lock=True,
-        favicon_path=TRACKIO_LOGO_PATH,
-        allowed_paths=[TRACKIO_LOGO_PATH],
+        favicon_path=TRACKIO_LOGO_DIR / "trackio_logo_light.png",
+        allowed_paths=[TRACKIO_LOGO_DIR],
     )
     base_url = share_url + "/" if share_url else url
     dashboard_url = base_url + f"?project={project}" if project else base_url
