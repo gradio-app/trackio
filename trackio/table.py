0db8868fa8c9bcfd959c66c27aae49c7fdba95e5
Zach Nation <zach@huggingface.co>
2025-08-28 03:04:35 -0700

    Add docstrings for Table

--- trackio/table.py
+++ trackio/table.py
@@ -1,4 +1,6 @@
-from typing import Literal, Optional
+from typing import Any, List, Literal, Optional, Union
+
+from pandas import DataFrame
 
 
 class Table:
@@ -6,17 +8,33 @@ class Table:
 
     def __init__(
         self,
-        columns=None,
-        data=None,
-        rows=None,
-        dataframe=None,
-        dtype=None,
-        optional=True,
-        allow_mixed_types=False,
+        columns: Optional[List[str]] = None,
+        data: Optional[List[List[Any]]] = None,
+        dataframe: Optional[DataFrame] = None,
+        rows: Optional[List[List[Any]]] = None,
+        optional: Union[bool, List[bool]] = True,
+        allow_mixed_types: bool = False,
         log_mode: Optional[
             Literal["IMMUTABLE", "MUTABLE", "INCREMENTAL"]
         ] = "IMMUTABLE",
     ):
+        """
+        Initializes a Table object.
+
+        Args:
+            columns: (List[str]) Names of the columns in the table.
+                Optional if `data` is provided.
+                Not expected if `dataframe` is provided.
+                Currently ignored.
+            data: (List[List[any]]) 2D row-oriented array of values.
+            dataframe: (pandas.DataFrame) DataFrame object used to create the table.
+                When set, `data` and `columns` arguments are ignored.
+            rows: (List[List[any]]) Currently ignored.
+            optional: (Union[bool,List[bool]]) Currently ignored.
+            allow_mixed_types: (bool) Currently ignored.
+            log_mode: (Optional[Literal["IMMUTABLE", "MUTABLE", "INCREMENTAL"]]) Currently ignored.
+        """
+
         # TODO: implement support for columns, dtype, optional, allow_mixed_types, and log_mode.
         # for now (like `rows`) they are included for API compat but don't do anything.
 
