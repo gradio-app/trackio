cb6fe1b55c5d952cf3fb274835b7fbf562a1ecb1
Quentin Gallouédec <gallouedec.quentin@gmail.com>
2025-09-02 16:30:35 +0000

    fix/add doc

--- trackio/table.py
+++ trackio/table.py
@@ -4,6 +4,28 @@ from pandas import DataFrame
 
 
 class Table:
+    """
+    Initializes a Table object.
+
+    Args:
+        columns (`list[str]`, *optional*, defaults to `None`):
+            Names of the columns in the table. Optional if `data` is provided. Not
+            expected if `dataframe` is provided. Currently ignored.
+        data (`list[list[Any]]`, *optional*, defaults to `None`):
+            2D row-oriented array of values.
+        dataframe (`pandas.`DataFrame``, *optional*, defaults to `None`):
+            DataFrame object used to create the table. When set, `data` and `columns`
+            arguments are ignored.
+        rows (`list[list[any]]`, *optional*, defaults to `None`):
+            Currently ignored.
+        optional (`bool` or `list[bool]`, *optional*, defaults to `True`):
+            Currently ignored.
+        allow_mixed_types (`bool`, *optional*, defaults to `False`):
+            Currently ignored.
+        log_mode: (`Literal["IMMUTABLE", "MUTABLE", "INCREMENTAL"]` or `None`, *optional*, defaults to `"IMMUTABLE"`):
+            Currently ignored.
+    """
+
     TYPE = "trackio.table"
 
     def __init__(
@@ -18,28 +40,6 @@ class Table:
             Literal["IMMUTABLE", "MUTABLE", "INCREMENTAL"]
         ] = "IMMUTABLE",
     ):
-        """
-        Initializes a Table object.
-
-        Args:
-            columns (`list[str]`, *optional*, defaults to `None`):
-                Names of the columns in the table. Optional if `data` is provided. Not
-                expected if `dataframe` is provided. Currently ignored.
-            data (`list[list[Any]]`, *optional*, defaults to `None`):
-                2D row-oriented array of values.
-            dataframe (`pandas.`DataFrame``, *optional*, defaults to `None`):
-                DataFrame object used to create the table. When set, `data` and
-                `columns` arguments are ignored.
-            rows (`list[list[any]]`, *optional*, defaults to `None`):
-                Currently ignored.
-            optional (`bool` or `list[bool]`, *optional*, defaults to `True`):
-                Currently ignored.
-            allow_mixed_types (`bool`, *optional*, defaults to `False`):
-                Currently ignored.
-            log_mode: (`Literal["IMMUTABLE", "MUTABLE", "INCREMENTAL"]` or `None`, *optional*, defaults to `"IMMUTABLE"`):
-                Currently ignored.
-        """
-
         # TODO: implement support for columns, dtype, optional, allow_mixed_types, and log_mode.
         # for now (like `rows`) they are included for API compat but don't do anything.
 
