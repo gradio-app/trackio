587518cc3a3f9dc19885764c988d5c3dad69248c
Abubakar Abid <abubakar@huggingface.co>
2025-09-04 13:05:24 -0700

    Remove inline comments

--- trackio/deploy.py
+++ trackio/deploy.py
@@ -45,9 +45,7 @@ def deploy_as_space(
     space_id: str,
     dataset_id: str | None = None,
 ):
-    if (
-        os.getenv("SYSTEM") == "spaces"
-    ):  # in case a repo with this function is uploaded to spaces
+    if os.getenv("SYSTEM") == "spaces":
         return
 
     trackio_path = files("trackio")
@@ -62,7 +60,7 @@ def deploy_as_space(
             exist_ok=True,
         )
     except HTTPError as e:
-        if e.response.status_code in [401, 403]:  # unauthorized or forbidden
+        if e.response.status_code in [401, 403]:
             print("Need 'write' access token to create a Spaces repo.")
             huggingface_hub.login(add_to_git_credential=False)
             huggingface_hub.create_repo(
@@ -158,7 +156,7 @@ def create_space_if_not_exists(
     except RepositoryNotFoundError:
         pass
     except HTTPError as e:
-        if e.response.status_code in [401, 403]:  # unauthorized or forbidden
+        if e.response.status_code in [401, 403]:
             print("Need 'write' access token to create a Spaces repo.")
             huggingface_hub.login(add_to_git_credential=False)
             huggingface_hub.add_space_variable(
