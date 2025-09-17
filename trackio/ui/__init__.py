0281d183b2323f5d2b166bda96ac398d1daabc6e
Quentin Gallouédec <gallouedec.quentin@gmail.com>
2025-09-17 14:34:45 +0000

    fix import

--- trackio/ui/__init__.py
+++ trackio/ui/__init__.py
@@ -1,4 +1,8 @@
-from trackio.ui.main import demo
-from trackio.ui.runs import run_page
+try:
+    from trackio.ui.main import demo
+    from trackio.ui.runs import run_page
+except ImportError:
+    from ui.main import demo
+    from ui.runs import run_page
 
 __all__ = ["demo", "run_page"]
