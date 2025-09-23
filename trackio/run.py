19f484d9cf4f8995ae95d3eaee410173a3e46fa3
Saba Noorassa <saba@xethub.com>
2025-09-23 00:20:58 -0700

    only add group if not None

--- trackio/run.py
+++ trackio/run.py
@@ -46,7 +46,8 @@ class Run:
 
         self.config["_Username"] = self._get_username()
         self.config["_Created"] = datetime.now(timezone.utc).isoformat()
-        self.config["_Group"] = self.group
+        if self.group is not None:
+            self.config["_Group"] = self.group
 
         self._queued_logs: list[LogEntry] = []
         self._queued_uploads: list[UploadEntry] = []
