fe09b721533a484855ce0d8e98b072e18dab9e95
Abubakar Abid <islamrealm@gmail.com>
2026-04-09 11:12:18 -0700

    Remove added freeze tests

--- tests/unit/test_deploy.py
+++ tests/unit/test_deploy.py
@@ -4,10 +4,7 @@ from unittest.mock import patch
 from huggingface_hub import Volume
 
 from trackio import deploy
-from trackio.bucket_storage import (
-    _list_bucket_file_paths,
-    export_from_bucket_for_static,
-)
+from trackio.bucket_storage import _list_bucket_file_paths
 
 
 @patch("trackio.deploy.huggingface_hub.HfApi")
@@ -48,30 +45,3 @@ def test_list_bucket_file_paths_uses_list_bucket_tree(mock_list_bucket_tree):
         prefix="trackio/media/proj/",
         recursive=True,
     )
-
-
-@patch("trackio.bucket_storage.huggingface_hub.download_bucket_files")
-@patch("trackio.bucket_storage.copy_files")
-@patch("trackio.bucket_storage._export_and_upload_static")
-@patch("trackio.bucket_storage._list_bucket_file_paths")
-@patch("trackio.bucket_storage._download_db_from_bucket")
-def test_export_from_bucket_for_static_copies_media_server_side(
-    mock_download_db,
-    mock_list_bucket_file_paths,
-    mock_export_and_upload_static,
-    mock_copy_files,
-    mock_download_bucket_files,
-):
-    mock_download_db.return_value = True
-    mock_list_bucket_file_paths.return_value = ["trackio/media/proj/image.png"]
-
-    export_from_bucket_for_static(
-        "abidlabs/source-bucket", "abidlabs/dest-bucket", "proj"
-    )
-
-    mock_export_and_upload_static.assert_called_once()
-    mock_copy_files.assert_called_once_with(
-        "hf://buckets/abidlabs/source-bucket/trackio/media/proj/",
-        "hf://buckets/abidlabs/dest-bucket/media/",
-    )
-    mock_download_bucket_files.assert_not_called()
